ed7a0a59dae4464ee5bf92610442a5f6a5d971a2
engel80 <prettyhip@gmail.com>
2022-05-24 01:55:17 +0900

    .

--- ecs-iam-role/bin/index.ts
+++ ecs-iam-role/bin/index.ts
@@ -3,7 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 
 import { EcsIamRoleStack } from '../lib/ecs-iam-role-stack';
 
-
 const app = new cdk.App();
 const env = {
     account: process.env.CDK_DEFAULT_ACCOUNT,
